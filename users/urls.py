648f27a7b8683d114c9b9f08e0f8537e4523ec95
xu <1137847433@qq.com>
2019-06-21 20:40:27 +0800

    创建视图

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+# urlpatterns是被django自动识别的路由列表变量
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    # 每个路由信息都需要使用url函数来构造
+    # url(路径, 视图)
+    url(r'^index/$', views.index),
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    '''
+    index视图
+    :param request:  包含了请求信息的请求对象
+    :return: 响应对象
+    '''
+    return HttpResponse("hello world")
