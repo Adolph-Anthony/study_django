7a281e40f871e6f1b4256671f72d1b52a4bd8e65
xu <1137847433@qq.com>
2019-06-22 18:55:40 +0800

    提取查询字符串参数

--- reqresp/urls.py
+++ reqresp/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
 
     # url(r'^weather/([a-z]+)/(\d{4})$',views.weather)
     #下面这个办法可以不按参数顺序传入weather函数
-    url(r'^weather/(?P<city>[a-z]+)/(?P<year>\d{4})$',views.weather)
+    url(r'^weather/(?P<city>[a-z]+)/(?P<year>\d{4})/$',views.weather)
 ]
--- reqresp/views.py
+++ reqresp/views.py
@@ -6,8 +6,18 @@ from django.shortcuts import render
 #     print(city)
 #     print(year)
 #     return HttpResponse("OK")
-def weather(request,year,city):
-    print(city)
+#
 
-    print(year)
+# /weather/beijing/2018/?a=1&b=2&a=3
+def weather(request,year,city):
+    # print(city)
+    # print(year)
+    # 查询字符串参数
+    #获取到多值时返回最后一个值 a = 3
+    a = request.GET.get('a')
+    b = request.GET.get('b')
+    alist = request.GET.getlist('a')
+
+    print(a,b,alist)
+    # HttpRequest对象的属性GET、POST都是QueryDict类型的对象
     return HttpResponse("OK")
