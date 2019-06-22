e3fbcb62b3729342705bea664e89e3d72cf7ba08
xu <1137847433@qq.com>
2019-06-22 20:10:54 +0800

    获取请求体数据

--- reqresp/views.py
+++ reqresp/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+import json
 # Create your views here.
 # def weather(request,city,year):
 #     print(city)
@@ -19,5 +19,25 @@ def weather(request,year,city):
     alist = request.GET.getlist('a')
 
     print(a,b,alist)
+    print('-----------------')
+
+    # 表单格式
+    c = request.POST.get('c')
+    d = request.POST.get('d')
+    clist = request.POST.getlist('c')
+    print(c,d,clist)
+    print(request.body)
+
+    # JSON
+    # json_bytes = request.body
+    # json_str = json_bytes.decode()
+    #前端发来的请求的字典数据
+    # req_dict = json.loads(json_str) #跟python3.6  str  bytes   python3.5只支持str
+    # c = req_dict.get('c')
+    # d = req_dict.get('d')
+    # print(c)
+    # print(d)
+
+
     # HttpRequest对象的属性GET、POST都是QueryDict类型的对象
     return HttpResponse("OK")
