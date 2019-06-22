27add0842cdf70436187411fe4f560e50e515558
xu <1137847433@qq.com>
2019-06-22 20:45:10 +0800

    请求头测试

--- reqresp/views.py
+++ reqresp/views.py
@@ -22,22 +22,24 @@ def weather(request,year,city):
     print('-----------------')
 
     # 表单格式
-    c = request.POST.get('c')
-    d = request.POST.get('d')
-    clist = request.POST.getlist('c')
-    print(c,d,clist)
-    print(request.body)
+    # c = request.POST.get('c')
+    # d = request.POST.get('d')
+    # clist = request.POST.getlist('c')
+    # print(c,d,clist)
+    # #请求体里获取   c=4&c=5&d=6
+    # print(request.body)
 
     # JSON
-    # json_bytes = request.body
-    # json_str = json_bytes.decode()
-    #前端发来的请求的字典数据
-    # req_dict = json.loads(json_str) #跟python3.6  str  bytes   python3.5只支持str
-    # c = req_dict.get('c')
-    # d = req_dict.get('d')
-    # print(c)
-    # print(d)
-
+    json_bytes = request.body
+    json_str = json_bytes.decode()
+    # 前端发来的请求的字典数据
+    req_dict = json.loads(json_str) #跟python3.6  str  bytes   python3.5只支持str
+    c = req_dict.get('c')
+    d = req_dict.get('d')
+    print(c)
+    print(d)
+    #获取请求头
+    print(request.META.get('CONTENT_LENGTH'))
 
     # HttpRequest对象的属性GET、POST都是QueryDict类型的对象
     return HttpResponse("OK")
