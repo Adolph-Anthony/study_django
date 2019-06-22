49d0ecb5307c9ab19e031cab952b730d8acf0b1e
xu <1137847433@qq.com>
2019-06-22 15:58:51 +0800

    URL提取参数

---
+++ reqresp/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+# def weather(request,city,year):
+#     print(city)
+#     print(year)
+#     return HttpResponse("OK")
+def weather(request,year,city):
+    print(city)
+
+    print(year)
+    return HttpResponse("OK")
