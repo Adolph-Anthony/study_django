ba35d5899887407c3536e141e76b431bc7b92157
xu <1137847433@qq.com>
2019-06-27 10:21:21 +0800

    自定义Admin管理类

--- booktest/admin.py
+++ booktest/admin.py
@@ -3,6 +3,17 @@ from django.contrib import admin
 # Register your models here.
 from booktest.models import BookInfo, HeroInfo
 
+@admin.register(BookInfo)
+class BookInfoAdmin(admin.ModelAdmin):
+    '''样式调整'''
+    pass
+
+@admin.register(HeroInfo)
+class HeroInfoAdmin(admin.ModelAdmin):
+    '''样式调整'''
+    pass
+
+
 #注册管理模型类
 admin.site.register(BookInfo)
 admin.site.register(HeroInfo)
