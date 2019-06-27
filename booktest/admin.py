ac217cc8e52a0d4ee1947f3861d726c4e394e231
xu <1137847433@qq.com>
2019-06-27 09:38:53 +0800

    使用Admin站点的预设值

--- booktest/admin.py
+++ booktest/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from booktest.models import BookInfo, HeroInfo
+
+#注册管理模型类
+admin.site.register(BookInfo)
+admin.site.register(HeroInfo)
